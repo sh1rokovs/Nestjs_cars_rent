c11edb760cb863bf653763d2957170b77a9669d2
sh1rokovs <hilallod@yandex.ru>
2023-05-19 13:43:51 +0300

    up:db && env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,7 @@ import { AuthModule } from './app/auth/auth.module';
 import { CarsModule } from './app/cars/cars.module';
 import { RolesModule } from './app/roles/roles.module';
 import { UsersModule } from './app/users/users.module';
-import { Car } from './app/cars/entity/car.entity';
-import { Role } from './app/roles/entity/role.entity';
-import { User } from './app/users/entity/user.entity';
+import { appDataSource } from './database/config/typeorm.config';
 
 @Module({
   controllers: [],
@@ -23,16 +21,7 @@ import { User } from './app/users/entity/user.entity';
     ServeStaticModule.forRoot({
       rootPath: path.resolve(__dirname, 'static'),
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [Role, User, Car],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(appDataSource.options),
     UsersModule,
     RolesModule,
     AuthModule,
---
+++ src/database/config/typeorm.config.ts
@@ -0,0 +1,31 @@
+import { ConfigService } from '@nestjs/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { config } from 'dotenv';
+import { join } from 'path';
+
+config({ path: join(process.cwd(), '.env') });
+
+const configService = new ConfigService();
+
+const options = (): DataSourceOptions => {
+  const url = configService.get('DATABASE_URL');
+
+  if (!url) {
+    throw new Error('Database url is empty');
+  }
+
+  return {
+    url,
+    type: 'postgres',
+    schema: 'public',
+    logging: configService.get('DB_LOGS') === 'true',
+    entities: [],
+    migrations: [
+      join(process.cwd(), 'database', 'migrations', '**', 'migrations.ts'),
+    ],
+    migrationsRun: true,
+    migrationsTableName: 'migrations',
+  };
+};
+
+export const appDataSource = new DataSource(options());

63114c1c67faaa4e8ca54e761cc4007045c60fcb
sh1rokovs <hilallod@yandex.ru>
2023-05-19 20:12:18 +0300

    style fix

--- src/database/config/data-source.ts
+++ src/database/config/data-source.ts
@@ -1,7 +1,6 @@
 import { ConfigService } from '@nestjs/config';
 import { config } from 'dotenv';
 import { join } from 'path';
-import 'reflect-metadata';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { SeederOptions } from 'typeorm-extension';
 
--- src/database/factories/user.factory.ts
+++ src/database/factories/user.factory.ts
@@ -4,6 +4,7 @@ import { User } from '../entities/user.entity';
 
 export default setSeederFactory(User, (faker) => {
   const user = new User();
+
   user.username = faker.internet.userName();
   user.password = faker.internet.password();
   user.email = faker.internet.email(user.username, 'example');
